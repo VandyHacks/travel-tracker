f079a458eeccd4b5e040be82b76d430a0fde724b
Benjamin Cooper <bencooper222@gmail.com>
2017-02-16 19:01:38 -0600

    Basic front end done!

---
+++ website/script.js
@@ -0,0 +1,5 @@
+var map = L.map('map-container').setView([39.833333, -98.583333], 5);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
